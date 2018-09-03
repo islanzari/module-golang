d66e216009e97c6502a35cab32374f1ec717d5b1
Oskar Nowak <on@ges.sh>
2018-09-03 14:25:05 +0200

    Add function downcase

---
+++ ex01-downcase/downcase.go
@@ -0,0 +1,15 @@
+package downcase
+
+func Downcase(s string) (string, error) {
+	b := make([]byte, len(s))
+	for i := range b {
+		c := s[i]
+		if c >= 'A' && c <= 'Z' {
+			c += 'a' - 'A'
+		}
+		b[i] = c
+	}
+
+	return string(b), nil
+
+}
