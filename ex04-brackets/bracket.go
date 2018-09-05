0a1c6c3c3ecbb7eb76572b5cc2276cf0e54bcbf0
Oskar Nowak <on@ges.sh>
2018-09-05 11:32:16 +0200

    Add commend

--- ex04-brackets/bracket.go
+++ ex04-brackets/bracket.go
@@ -1,5 +1,6 @@
 package brackets
 
+//Bracket return true or false depends on the string
 func Bracket(s string) (bool, error) {
 
 	output, err := BraceCounting(s)
@@ -7,13 +8,13 @@ func Bracket(s string) (bool, error) {
 	return output, err
 }
 
+// BraceCounting check if symbols in string are corect and return bool
 func BraceCounting(s string) (bool, error) {
 	var str []rune
 	for _, symbol := range s {
 		switch symbol {
 		case '{', '[', '(':
 			str = append(str, symbol)
-
 		case '}', ']', ')':
 			if len(str) > 0 {
 				if symbol == '}' && str[len(str)-1] == '{' {
@@ -25,7 +26,6 @@ func BraceCounting(s string) (bool, error) {
 				} else {
 					return false, nil
 				}
-
 			}
 		}
 	}
